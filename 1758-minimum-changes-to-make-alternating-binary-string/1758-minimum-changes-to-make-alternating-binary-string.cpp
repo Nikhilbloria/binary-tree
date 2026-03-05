0f16dc52fa62eaf98f021b8152af6aa9d9f564f3
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-03-05 14:43:22 +0530

    Time: 0 ms (100%), Space: 9.5 MB (20.96%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,9 +1,43 @@
+// class Solution {
+// public:
+//     int minOperations(string s) {
+//         int count = 0;
+//         for(int i = 0; i < s.size(); i++)
+//             if(s[i] != (i % 2 + '0')) count++;
+//         return min(count, (int)s.size() - count);
+//     }
+// };
+// class Solution {
+// public:
+//     int minOperations(string s) {
+//         int count = 0, n = s.length();
+//         for (int i = 0; i < n; i++)
+//             count += (s[i] ^ i) & 1;
+          
+//         return min(count, n - count);
+//     }
+// };
 class Solution {
 public:
     int minOperations(string s) {
-        int count = 0;
-        for(int i = 0; i < s.size(); i++)
-            if(s[i] != (i % 2 + '0')) count++;
-        return min(count, (int)s.size() - count);
+        int n = s.length();
+        int count1=0;
+        int count2 =0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                if(s[i]=='0'){
+                    count2++;
+                }else{
+                    count1++;
+                }
+            }else{
+                if(s[i]=='1'){
+                    count2++;
+                }else{
+                    count1++;
+                }
+            }
+        }
+        return min(count2,count1);
     }
 };
