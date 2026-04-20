b5d9dc2548d69c9c4e4f1521bb8c8fdfe9cba838
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-04-21 00:27:10 +0530

    Time: 87 ms (23.23%), Space: 93.6 MB (87.27%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,15 +1,39 @@
+// class Solution {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         unordered_map<int,int>mp;
+//         for(int i=0;i<n;i++){
+//             if(mp.find(nums[i])!=mp.end() &&
+//             abs(mp[nums[i]]-i)<=k){
+//                 return true;
+//             }else{
+//                 mp[nums[i]] = i;
+//             }
+//         }return false;
+//     }
+// };
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            if(mp.find(nums[i])!=mp.end() &&
-            abs(mp[nums[i]]-i)<=k){
+        unordered_set<int> st;
+
+        for(int i = 0; i < nums.size(); i++) {
+            
+            // Step 1: Check duplicate
+            if(st.find(nums[i]) != st.end()) {
                 return true;
-            }else{
-                mp[nums[i]] = i;
             }
-        }return false;
+
+            // Step 2: Insert current element
+            st.insert(nums[i]);
+
+            // Step 3: Maintain window size k
+            if(st.size() > k) {
+                st.erase(nums[i - k]);
+            }
+        }
+
+        return false;
     }
 };
