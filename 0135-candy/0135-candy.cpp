a1ac50688a0f56c7ddc841b575dfa6503fd32a5f
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-04-29 00:29:09 +0530

    Time: 1 ms (32.67%), Space: 22.8 MB (69.07%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -25,20 +25,20 @@ public:
         int n = ratings.size();
         vector<int> candies(n, 1);
 
-        for (int i = 1; i < n; i++) 
+        for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1])
                 candies[i] = candies[i - 1] + 1;
-        
+        }
         for (int i = n - 1; i > 0; i--) {
             if (ratings[i - 1] > ratings[i])
                 candies[i - 1] = max(candies[i] + 1, candies[i - 1]);
             cnt += candies[i - 1];
         }
-        // return cnt + candies[n - 1]; OR
-        int total = 0;
-        for(int c:candies){
-            total+=c;
-        }
-        return total;
+         return cnt + candies[n - 1]; 
+        // int total = 0;
+        // for(int c:candies){
+        //     total+=c;
+        // }
+        // return total;
     }
 };
