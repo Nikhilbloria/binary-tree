36e05b92ff9193ae5ec46625d9e5e35c053f9766
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-05-01 02:09:32 +0530

    Time: 22 ms (19.32%), Space: 14.3 MB (29.43%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -16,3 +16,16 @@ public:
         return maxi;
     }
 };
+// unordered_map<char, int> mp;
+// int left = 0, maxi = 0;
+
+// for(int right = 0; right < s.size(); right++) {
+
+//     if(mp.find(s[right]) != mp.end()) {
+//         left = max(left, mp[s[right]] + 1);
+//     }
+
+//     mp[s[right]] = right;
+
+//     maxi = max(maxi, right - left + 1);
+// }
